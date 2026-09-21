a153c315fcde3af9f2365f7c77bf2b7a6e5fcdfd
joa214 <ruejoana@gmail.com>
2026-09-21 19:21:14 +0200

    index connection

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
+import React from "react";
+import Portfolio from "C:\Users\joanaa\my-portfolio\index.html";
+
 function App() {
   return (
     <div className="App">
-      <my-portfolio />
+      <index.html/>
     </div>
   );
 }
+
+export default App;
